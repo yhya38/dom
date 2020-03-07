488fe8761cc2f49500be733e304815a2b6974fb9
yk2217 <yahiyakhan2217@gmail.com>
2020-03-07 19:26:52 +0530

    js modified

--- index.js
+++ index.js
@@ -7,17 +7,29 @@ function myFun() {
     document.getElementById("message").innerHTML = "First name cannot be empty";
     return false;
   }
+  else {
+    document.getElementById("message").innerHTML = "";
+  }
   if (b == "") {
     document.getElementById("message2").innerHTML = "Last name cannot  be empty";
     return false;
+  }
+  else {
+    document.getElementById("message2").innerHTML = "";
   }
   if (c == "") {
     document.getElementById("message3").innerHTML = "looks like this is not an email";
     return false
   }
+  else {
+    document.getElementById("message3").innerHTML = "";
+  }
   if (d == "") {
     document.getElementById("message4").innerHTML = "invalid password";
     return false
+  }
+  else {
+    document.getElementById("message4").innerHTML = "";
   }
   
 }
