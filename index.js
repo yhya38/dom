2999d8f2aac2c26046649ddb022cd4ab8135cdc3
yk2217 <yahiyakhan2217@gmail.com>
2020-03-07 20:43:52 +0530

    input modified

--- index.js
+++ index.js
@@ -5,31 +5,54 @@ function myFun() {
   var d = document.getElementById("pass").value;
   if (a == "") {
     document.getElementById("message").innerHTML = "First name cannot be empty";
+    document.getElementById("first_name").style.border =
+      "1px solid hsl(0, 100%, 74%)";
     return false;
-  }
-  else {
+  } else if (a == "fill") {
     document.getElementById("message").innerHTML = "";
+  } else {
+    document.getElementById("message").innerHTML = "";
+    document.getElementById("first_name").style.border ="";
   }
   if (b == "") {
-    document.getElementById("message2").innerHTML = "Last name cannot  be empty";
+    document.getElementById("message2").innerHTML =
+      "Last name cannot  be empty";
+    document.getElementById("last_name").style.border =
+      "1px solid hsl(0, 100%, 74%)";
+
     return false;
-  }
-  else {
+  } else if (b == "fill") {
+    document.getElementById("message").innerHTML = "";
+  } else {
     document.getElementById("message2").innerHTML = "";
+    document.getElementById("last_name").style.border = "";
+
   }
   if (c == "") {
-    document.getElementById("message3").innerHTML = "looks like this is not an email";
-    return false
-  }
-  else {
+    document.getElementById("message3").innerHTML =
+      "looks like this is not an email";
+    document.getElementById("email").style.border =
+      "1px solid hsl(0, 100%, 74%)";
+
+    return false;
+  } else if (c == "fill") {
+    document.getElementById("message").innerHTML = "";
+  } else {
     document.getElementById("message3").innerHTML = "";
+    document.getElementById("email").style.border = "";
+
   }
   if (d == "") {
     document.getElementById("message4").innerHTML = "invalid password";
-    return false
-  }
-  else {
+    document.getElementById("pass").style.border =
+      "1px solid hsl(0, 100%, 74%)";
+
+    return false;
+  } else if (d == "fill") {
+    document.getElementById("message").innerHTML = "";
+  } else {
     document.getElementById("message4").innerHTML = "";
+    document.getElementById("pass").style.border = "";
+
   }
-  
 }
