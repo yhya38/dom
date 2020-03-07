8085d2e64e7304b5a31827c44c653946e83f31b8
yk2217 <yahiyakhan2217@gmail.com>
2020-03-07 21:04:08 +0530

    re-modified

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function myFun() {
 
     return false;
   } else if (b == "fill") {
-    document.getElementById("message").innerHTML = "";
+    document.getElementById("message2").innerHTML = "";
   } else {
     document.getElementById("message2").innerHTML = "";
     document.getElementById("last_name").style.border = "";
@@ -36,20 +36,22 @@ function myFun() {
 
     return false;
   } else if (c == "fill") {
-    document.getElementById("message").innerHTML = "";
+    document.getElementById("message3").innerHTML = "";
   } else {
     document.getElementById("message3").innerHTML = "";
     document.getElementById("email").style.border = "";
 
   }
   if (d == "") {
-    document.getElementById("message4").innerHTML = "invalid password";
+    document.getElementById("message4").innerHTML = "password cannot be empty";
     document.getElementById("pass").style.border =
       "1px solid hsl(0, 100%, 74%)";
 
     return false;
   } else if (d == "fill") {
-    document.getElementById("message").innerHTML = "";
+    document.getElementById("message4").innerHTML = "";
+    document.getElementById("message4").innerHTML = "invalid password";
+
   } else {
     document.getElementById("message4").innerHTML = "";
     document.getElementById("pass").style.border = "";
